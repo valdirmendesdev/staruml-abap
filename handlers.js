2cea854b0f8516d97f6ca8f5a31e6fb9174dbab1
Valdir Mendes <valdir.mfjesus@yahoo.com.br>
2021-01-30 17:10:18 -0300

    Adding dialogs

--- handlers.js
+++ handlers.js
@@ -5,6 +5,21 @@
  */
 exports.handleGenerate = async () => {
 
-  const { buttonId, returnValue: base } = await app.elementPickerDialog.showDialog('Select a base model to generate codes', null, type.UMLPackage);
+  const { buttonId: clickedButton, returnValue: baseModel } = await app.elementPickerDialog.showDialog('Select a base model to generate codes', null, type.UMLPackage);
+
+  if (clickedButton !== 'ok') {
+    return
+  }
+
+  if (!baseModel) {
+    app.dialogs.showAlertDialog('You must select a base model!');
+    return
+  }
+
+  const filesPath = app.dialogs.showOpenDialog('Select a folder where generated codes to be located', null, null, { properties: ['openDirectory'] });
+  if (!filesPath) {
+    app.dialogs.showAlertDialog('You must select a folder for the files generation!');
+    return
+  }
 
 }
